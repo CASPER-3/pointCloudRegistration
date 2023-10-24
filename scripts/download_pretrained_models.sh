627a75005f292a5240e0e37c5d2882bb52607b02
WangZx <wzk1205434581@hotmail.com>
2023-10-24 14:42:51 +0800

    first commit

---
+++ scripts/download_pretrained_models.sh
@@ -0,0 +1,6 @@
+mkdir pretrained
+cd pretrained
+wget --no-check-certificate --show-progress https://share.phys.ethz.ch/~gsg/LMPR/pretrained_models/pretrained_models_LMPR.zip
+unzip pretrained_models_LMPR.zip
+rm pretrained_models_LMPR.zip
+cd ..
